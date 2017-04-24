16bfc4e9ad9127eeebd43de62300c4193680e3cb
Tamás Perlaki <pt1029@gmail.com>
2017-04-24 18:35:32 +0200

    Removed dummy bot from the project to move it to a different project.

--- src/bot.js
+++
@@ -1,9 +0,0 @@
-const irc = require('irc');
-
-const bot = new irc.Client('irc.chat.twitch.tv', 'tankika', {
-    autoConnect: false,
-    sasl: true
-});
-
-bot.addListener('raw', m => console.log(m));
-bot.connect();asd
