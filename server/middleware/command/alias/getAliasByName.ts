bd29f801b31611b108cc90d70f382180b765997e
Tamás Perlaki <pt1029@gmail.com>
2017-12-03 23:09:10 +0100

    security documentation

--- server/middleware/command/alias/getAliasByName.ts
+++ server/middleware/command/alias/getAliasByName.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { Alias } from '../../../../models/alias';
+import * as sanitize from 'mongo-sanitize';
 
 export default () => {
   return (req: Request, res: Response, next: NextFunction) => {
     Alias
       .findOne({
         'user': req.session.userId,
-        'name': req.body.name
+        'name': sanitize(req.body.name)
        })
       .then(alias => {
         res.locals.alias = alias;
